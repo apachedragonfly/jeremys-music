1e6f1621e30e95d5c7724f371ee0a78f9f8b01f0
apachedragonfly <100011406+apachedragonfly@users.noreply.github.com>
2024-12-25 15:13:02 -0330

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
-  site: "https://jeremys-music.vercel.app",
+  site: "https://astro-nano-demo.vercel.app",
   integrations: [mdx(), sitemap(), tailwind()],
 });
