fb97bf59b0b6456ce0d14082945659114026c4c5
apachedragonfly <100011406+apachedragonfly@users.noreply.github.com>
2025-05-17 01:37:14 -0230

    Update consts.ts

--- src/consts.ts
+++ src/consts.ts
@@ -3,7 +3,7 @@ import type { Site, Metadata, Socials } from "@types";
 export const SITE: Site = {
   NAME: "Jeremy Craig Saunders",
   EMAIL: "",
-  NUM_POSTS_ON_HOMEPAGE: 2,
+  NUM_POSTS_ON_HOMEPAGE: 6,
   NUM_PROJECTS_ON_HOMEPAGE: 2,
   NUM_WORKS_ON_HOMEPAGE: 2,
 };
